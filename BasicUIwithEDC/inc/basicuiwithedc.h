c74c75d2cf8dda092905da76312424b9c3f0a9bc
mishu-cse <64586256+mishu-cse@users.noreply.github.com>
2022-02-12 12:32:32 -0800

    Add files via upload

---
+++ BasicUIwithEDC/inc/basicuiwithedc.h
@@ -0,0 +1,23 @@
+#ifndef __basicuiwithedc_H__
+#define __basicuiwithedc_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "basicuiwithedc"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.basicuiwithedc"
+#endif
+
+#define EDJ_FILE "edje/basicuiwithedc.edj"
+#define GRP_MAIN "main"
+
+
+#endif /* __basicuiwithedc_H__ */
